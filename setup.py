b944e70a476c435123f3e8b75632daa8a8e423b8
Akihiro Kuroiwa <akuroiwa@env-reform.com>
2023-02-23 19:15:38 +0900

    Version 0.0.5.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='chess_classification',
-    version='0.0.4',
+    version='0.0.5',
     url='https://github.com/akuroiwa/chess-classification',
     # # PyPI url
     # download_url='',
